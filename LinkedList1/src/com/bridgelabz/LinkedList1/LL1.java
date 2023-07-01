e11f7c629f5858848b9397cd717b775f38f188ca
Prince Kumar <kumarprince1407@gmail.com>
2023-07-01 06:25:55 +0530

    Added a file.

---
+++ LinkedList1/src/com/bridgelabz/LinkedList1/LL1.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.LinkedList1;
+
+public class LL1 {
+    public static void main(String[] args) {
+        System.out.println("Lets create a simple Linked List of 56, 30 and 70.");
+    }
+}
